4a4d12dc96c5bcb4bc72a5a33f76af81be250f12
clarrisobriko-lab <clarrisobriko@gmail.com>
2026-08-12 17:18:37 +0100

    feat: add SmartRecruiters to production registry

--- connectors/registry.py
+++ connectors/registry.py
@@ -2,8 +2,9 @@ from connectors.ashby import AshbyConnector
 from connectors.base import BaseConnector
 from connectors.greenhouse import GreenhouseConnector
 from connectors.lever import LeverConnector
+from connectors.smartrecruiters import SmartRecruitersConnector
 
 
 def get_connectors() -> list[BaseConnector]:
     """Return ATS connectors supported by the production application pipeline."""
-    return [GreenhouseConnector(), LeverConnector(), AshbyConnector()]
+    return [GreenhouseConnector(), LeverConnector(), AshbyConnector(), SmartRecruitersConnector()]
