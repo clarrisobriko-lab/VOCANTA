209ae90d91a4954c496bd2f6e2e9233a8230954c
clarrisobriko-lab <clarrisobriko@gmail.com>
2026-08-13 05:07:42 +0100

    feat: add Workday to production connector registry

--- connectors/registry.py
+++ connectors/registry.py
@@ -3,8 +3,15 @@ from connectors.base import BaseConnector
 from connectors.greenhouse import GreenhouseConnector
 from connectors.lever import LeverConnector
 from connectors.smartrecruiters import SmartRecruitersConnector
+from connectors.workday import WorkdayConnector
 
 
 def get_connectors() -> list[BaseConnector]:
     """Return ATS connectors supported by the production application pipeline."""
-    return [GreenhouseConnector(), LeverConnector(), AshbyConnector(), SmartRecruitersConnector()]
+    return [
+        GreenhouseConnector(),
+        LeverConnector(),
+        AshbyConnector(),
+        SmartRecruitersConnector(),
+        WorkdayConnector(),
+    ]
